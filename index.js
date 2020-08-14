0a50594cc52c031bff6257c7f3e5a8dc776dae66
hyr <764074931@qq.com>
2020-08-14 18:58:55 +0800

    svn逻辑更新

--- index.js
+++ index.js
@@ -10,38 +10,50 @@ const fs = require('fs');
 const ora = require('ora');
 const chalk = require('chalk');
 //默认存放h5项目的地方
-const project_path='h5project';
+const project_path = 'h5project';
 
 
 program
   .version(require('./package').version)
   .usage('<command> [options]')
+let argv;
+try {
+  // 通过 npm run dev 的方法执行的时候，参数更换获取方式
+  argv = JSON.parse(process.env.npm_config_argv).original;
+  program
+    .option('-d, --dirname <dirname>', '编译目录')//不写会报错 
+    .option('-s, --svnname <svnname>', 'svn上传目录')
+    .parse(argv);
 
+} catch (e) {
+  argv = process.argv;
+}
+
+// return;
 
 program
   .command('create <folderName>')
   .description('create a new project')
   //选择哪一个模板预设(最后个参数common为默认值)
-  .option('-p, --preset <presetName>', 'Skip prompts and use saved or remote preset','common')
+  .option('-p, --preset <presetName>', 'Skip prompts and use saved or remote preset', 'common')
   .option('-d, --default', 'Skip prompts and use default preset')
   .action((folderName, cmd) => {
     const options = cleanArgs(cmd);
     // console.log(projectName, templates[projectName]);
     // const { downloadurl } = templates[projectName];
-    console.log('optionsxxxxxx', options);
     // console.log('传入参数:' + minimist(process.argv.slice(3))._);
     if (minimist(process.argv.slice(3))._.length > 1) {
       console.log(chalk.yellow('\n ⚠️  检测到您输入了多个名称，将以第一个参数为项目名，舍弃后续参数哦'))
     }
-    console.log(project_path+"/"+folderName)
-    require('./lib/create')(project_path+"/"+folderName, options);
+    console.log(project_path + "/" + folderName)
+    require('./lib/create')(project_path + "/" + folderName, options);
     return;
   });
 
 // 创建页面命令
 program
   .command('pages')
-  .option('-p, --savepath <pathName>', 'savepath name','./')
+  .option('-p, --savepath <pathName>', 'savepath name', './')
   .description('create pages from resource.json')
   .action((cmd) => {
     const options = cleanArgs(cmd);
@@ -50,13 +62,14 @@ program
 
 //上传svn
 program
-  .command('svn <dirname>')
-  .option('-u, --username <username>', 'svn username','hyr')
-  .option('-p, --password <password>', 'svn password','123456')
+  .command('svn')
+  .option('-d, --svnname <dirname>', 'svn dirname', program.svnname)
+  .option('-u, --username <username>', 'svn username', 'hyr')
+  .option('-p, --password <password>', 'svn password', 'neone2018')
   .description('commit svn')
-  .action((dirname,cmd) => {
+  .action((cmd) => {
     const options = cleanArgs(cmd);
-    require('./lib/svn')(dirname,options);
+    require('./lib/svn')(options);
   })
 
 program.parse(process.argv);
